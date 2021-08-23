8239be48f4436eb4c5b5d41bf775a068174ed25c
Rakeshkjain12 <rakeshkjain12@gmail.com>
2021-08-23 16:04:16 +0530

    variable done

---
+++ Variable/test.js
@@ -0,0 +1,14 @@
+
+ //var data=200; //global variable
+function abc(){
+  
+ //var data1=300;   //local variable
+ window.value=500;
+ //document.writeln(data); 
+
+}
+
+function xyz(){
+	//document.writeln(data1);   
+    alert(window.value);
+}
